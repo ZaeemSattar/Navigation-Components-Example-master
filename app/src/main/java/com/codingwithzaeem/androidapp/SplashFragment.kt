232bf5052fce9558ac644ae7cbf46dcb6eeb777a
apple <1234>
2020-04-04 21:50:50 +0500

    Android Navigation component full example with splash screen and popTo & popToInclusive example

---
+++ app/src/main/java/com/codingwithzaeem/androidapp/SplashFragment.kt
@@ -0,0 +1,40 @@
+package com.codingwithzaeem.androidapp
+
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SplashFragment : Fragment() {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+
+        Handler().postDelayed({
+            navController.navigate(R.id.action_splashFragment_to_mainFragment)
+        }, 3000)
+    }
+
+
+}
